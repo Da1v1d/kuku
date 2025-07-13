98097cbadbf95e591eb4d1fba62d6d53b8a3409b
Da1v1d <davoadamyan64@gmail.com>
2025-07-13 21:01:09 +0400

    feat: update tailwindcss version

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
   ];
 
   return (
-    <div className="flex flex-col items-center justify-center  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-center  p-8 pb-20 gap-16 sm:p-20 font-(family-name:--font-geist-sans)">
       <Text className="text-4xl font-extrabold text-primary drop-shadow-primary">
         Kuku
       </Text>
--- src/shared/ui/sliders/audio-slider.tsx
+++ src/shared/ui/sliders/audio-slider.tsx
@@ -19,7 +19,7 @@ const AudioSlider = ({ duration, progress, ...props }: IProps) => {
           " h-[2px] group-hover:h-[4px] transition-height ease-in-out hover:cursor-pointer",
         thumb:
           "bg-primary invisible hover:cursor-pointer group-hover:visible bg-primary after:hidden w-4 h-4 bg-white",
-        // filler: "bg-gradient-to-r from-red-400  to-blue-400",
+        // filler: "bg-linear-to-r from-red-400  to-blue-400",
       }}
       minValue={0}
       size="sm"
